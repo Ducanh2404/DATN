6ba643bfe99f91337cb8828b21101bf5bc48e78a
Ducanh <tranducanh2404@gmail.com>
2023-11-01 17:12:00 +0700

    update

---
+++ project/lib/header/header_top.dart
@@ -0,0 +1,106 @@
+import 'dart:async';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HeaderTop extends StatefulWidget {
+  const HeaderTop({ Key? key }) : super(key: key);
+
+  @override
+  _HeaderTopState createState() => _HeaderTopState();
+}
+
+class _HeaderTopState extends State<HeaderTop> {
+   String? encodeQueryParameters(Map<String, String> params) {
+    return params.entries
+        .map((MapEntry<String, String> e) =>
+            '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+        .join('&');
+  }
+
+  Future<void> _sendEmail(String email) async {
+    final Uri emaillaunchUri = Uri(
+      scheme: 'mailto',
+      path: email,
+      query: encodeQueryParameters(<String, String>{
+        'subject': 'Gửi mail tới ${email}',
+      }),
+    );
+    await launchUrl(emaillaunchUri);
+  }
+
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+            alignment: Alignment.center,
+            width: double.infinity,
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+            decoration: const BoxDecoration(
+              color: Color(0xFF3278f6),
+            ),
+            child: SizedBox(
+              width: 1600,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                        border: Border(
+                      right: BorderSide(
+                        color: Color(0xFF2860c5),
+                        width: 2.0,
+                      ),
+                    )),
+                    child: TextButton.icon(
+                      onPressed: () => {
+                        setState(() {
+                          _makePhoneCall('tel:3459192');
+                        }),
+                      },
+                      icon: const Icon(
+                        Icons.call,
+                        color: Colors.white,
+                        size: 20.0,
+                      ),
+                      label: const Text(
+                        '(012) 345.9192',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  TextButton.icon(
+                    onPressed: () => {
+                      setState(() {
+                        _sendEmail('1951060533@e.tlu.edu.vn');
+                      }),
+                    },
+                    icon: const Icon(
+                      Icons.email,
+                      color: Colors.white,
+                      size: 20.0,
+                    ),
+                    label: const Text(
+                      '1951060533@e.tlu.edu.vn',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ));
+  }
+}
--- project/lib/layout.dart
+++ project/lib/layout.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:project/header/header_top.dart';
 
 /// a. creating StatefulWidget
 class AppLayout extends StatefulWidget {
@@ -14,31 +15,7 @@ class AppLayout extends StatefulWidget {
 
 /// b. Creating state for stateful widget
 class _AppLayoutState extends State {
-  String? encodeQueryParameters(Map<String, String> params) {
-    return params.entries
-        .map((MapEntry<String, String> e) =>
-            '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
-        .join('&');
-  }
-
-  Future<void> _sendEmail(String email) async {
-    final Uri emaillaunchUri = Uri(
-      scheme: 'mailto',
-      path: email,
-      query: encodeQueryParameters(<String, String>{
-        'subject': 'Gửi mail tới ${email}',
-      }),
-    );
-    await launchUrl(emaillaunchUri);
-  }
-
-  Future<void> _makePhoneCall(String phoneNumber) async {
-    final Uri launchUri = Uri(
-      scheme: 'tel',
-      path: phoneNumber,
-    );
-    await launchUrl(launchUri);
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -46,70 +23,7 @@ class _AppLayoutState extends State {
     return Scaffold(
       body: Column(children: [
         //Header top
-        Container(
-            alignment: Alignment.center,
-            width: double.infinity,
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-            decoration: const BoxDecoration(
-              color: Color(0xFF3278f6),
-            ),
-            child: SizedBox(
-              width: 1600,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                        border: Border(
-                      right: BorderSide(
-                        color: Color(0xFF2860c5),
-                        width: 2.0,
-                      ),
-                    )),
-                    child: TextButton.icon(
-                      onPressed: () => {
-                        setState(() {
-                          _makePhoneCall('tel:3459192');
-                        }),
-                      },
-                      icon: const Icon(
-                        Icons.call,
-                        color: Colors.white,
-                        size: 20.0,
-                      ),
-                      label: const Text(
-                        '(012) 345.9192',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  TextButton.icon(
-                    onPressed: () => {
-                      setState(() {
-                        _sendEmail('1951060533@e.tlu.edu.vn');
-                      }),
-                    },
-                    icon: const Icon(
-                      Icons.email,
-                      color: Colors.white,
-                      size: 20.0,
-                    ),
-                    label: const Text(
-                      '1951060533@e.tlu.edu.vn',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-              ),
-            )),
+        HeaderTop(),
         //header mid
         Container(
           alignment: Alignment.center,
