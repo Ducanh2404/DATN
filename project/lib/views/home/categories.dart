dacc8017f3ffa8a57318439bbe8fc35fab1b7045
Ducanh <tranducanh2404@gmail.com>
2023-11-09 17:11:23 +0700

    update

--- project/lib/views/home/categories.dart
+++ project/lib/views/home/categories.dart
@@ -20,25 +20,32 @@ class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 1600,
-      child: CarouselSlider(
-      items: imageUrls.map((imageUrl) {
-        return Builder(
-            builder: (BuildContext context) {
-                  return Container(
-                      width: MediaQuery.of(context).size.width/4,
-                      margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
-                      child: Row(children: [Image(image: AssetImage(imageUrl),width: 394,)],)
-        );},
-        );
-      }).toList(),
-      options: CarouselOptions(
-       padEnds :false,
-        height: 400.0,
-        viewportFraction: 1.0/4.0,
-        initialPage: 0,
-        enableInfiniteScroll: true,
-      ),
-    ));
+        width: 1600,
+        child: CarouselSlider(
+          items: imageUrls.map((imageUrl) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width / 4,
+                    margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
+                    child: Row(
+                      children: [
+                        Image(
+                          image: AssetImage(imageUrl),
+                          width: 394,
+                        )
+                      ],
+                    ));
+              },
+            );
+          }).toList(),
+          options: CarouselOptions(
+            padEnds: false,
+            height: 400,
+            viewportFraction: 1.0 / 4.0,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+          ),
+        ));
   }
 }
