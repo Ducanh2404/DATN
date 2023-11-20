cb094bf8d73e444ae02fcb0b1d763f0b05d6321c
Ducanh2404 <1951060533@e.tlu.edu.vn>
2023-11-20 23:10:46 +0700

    update

--- project/lib/widget/prod_details.dart
+++ project/lib/widget/prod_details.dart
@@ -21,28 +21,69 @@ class _ProductDetailsState extends State<ProductDetails> {
           _isHovered = false;
         });
       },
-      child: Container(
-        decoration: BoxDecoration(
-            border: Border.all(
-                color: _isHovered ? Color(0xFF3278f6) : Color(0xFFededed),
-                width: _isHovered ? 3.0 : 1.0)),
-        child: Column(
-          children: [
-            TextButton(
-              style: ButtonStyle(
-                overlayColor: TransparentButton(),
-                padding: MaterialStateProperty.all<EdgeInsets>(
-                    const EdgeInsets.all(0)),
-              ),
+      child: Stack(
+        children: [
+          TextButton(
+            style: ButtonStyle(
+              overlayColor: TransparentButton(),
+              padding: MaterialStateProperty.all<EdgeInsets>(
+                  const EdgeInsets.all(3)),
+            ),
+            child: Align(
+              alignment: Alignment.topCenter,
               child: Image(
                 image: AssetImage('img/product/product1.jpg'),
                 fit: BoxFit.contain,
               ),
-              onPressed: () {},
             ),
-            Text("123")
-          ],
-        ),
+            onPressed: () {},
+          ),
+          Container(
+              decoration: BoxDecoration(color: Color(0xFF8FFF00)),
+              padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+              child: Text(
+                "Best choice",
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700),
+              )),
+          Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: _isHovered ? double.infinity : 0.0,
+                height: _isHovered ? 3.0 : 0.0,
+                decoration: BoxDecoration(color: Color(0xFF3278f6)),
+              )),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                width: _isHovered ? 3.0 : 0.0,
+                height: _isHovered ? double.infinity : 0.0,
+                decoration: BoxDecoration(color: Color(0xFF3278f6)),
+              )),
+          Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                width: _isHovered ? 3.0 : 0.0,
+                height: _isHovered ? double.infinity : 0.0,
+                decoration: BoxDecoration(color: Color(0xFF3278f6)),
+              )),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: _isHovered ? double.infinity : 0.0,
+                height: _isHovered ? 3.0 : 0.0,
+                decoration: BoxDecoration(color: Color(0xFF3278f6)),
+              )),
+          Positioned.fill(
+            bottom: 0,
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                Text("123"),
+              ]),
+            ),
+          )
+        ],
       ),
     );
   }
