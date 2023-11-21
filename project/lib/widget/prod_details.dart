fc0ae9d8b7b85dad3291ab44a47e9787732a09c3
Ducanh <tranducanh2404@gmail.com>
2023-11-21 15:14:44 +0700

    update

--- project/lib/widget/prod_details.dart
+++ project/lib/widget/prod_details.dart
@@ -7,6 +7,12 @@ class ProductDetails extends StatefulWidget {
 
 class _ProductDetailsState extends State<ProductDetails> {
   bool _isHovered = false;
+  Color _textColor = Colors.black;
+  void _changeColor() {
+    setState(() {
+      _textColor = _textColor == Colors.black ? Colors.red : Colors.black;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,20 +29,23 @@ class _ProductDetailsState extends State<ProductDetails> {
       },
       child: Stack(
         children: [
-          TextButton(
-            style: ButtonStyle(
-              overlayColor: TransparentButton(),
-              padding: MaterialStateProperty.all<EdgeInsets>(
-                  const EdgeInsets.all(3)),
-            ),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Image(
-                image: AssetImage('img/product/product1.jpg'),
-                fit: BoxFit.contain,
+          Container(
+            height: 250,
+            child: TextButton(
+              style: ButtonStyle(
+                overlayColor: TransparentButton(),
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                    const EdgeInsets.all(3)),
               ),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Image(
+                  image: AssetImage('img/product/product1.jpg'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+              onPressed: () {},
             ),
-            onPressed: () {},
           ),
           Container(
               decoration: BoxDecoration(color: Color(0xFF8FFF00)),
@@ -46,43 +55,109 @@ class _ProductDetailsState extends State<ProductDetails> {
                 style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700),
               )),
           Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                      border: BorderDirectional(
+                          top: BorderSide(
+                              width: 1,
+                              style: BorderStyle.solid,
+                              color: Color(0xFFededed)))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.only(bottom: 8),
+                        height: 38,
+                        child: TextButton(
+                            style: ButtonStyle(
+                              overlayColor: TransparentButton(),
+                              padding: MaterialStateProperty.all<EdgeInsets>(
+                                  const EdgeInsets.all(0)),
+                            ),
+                            onPressed: () {},
+                            onHover: (value) {
+                              setState(() {
+                                  _changeColor();
+                              });
+                            },
+                            child: Text('PC Đỗ Đại Học 2023',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 16,
+                                    color: _textColor))),
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.star, size: 12, color: Color(0xFF8d94ac)),
+                          Icon(Icons.star, size: 12, color: Color(0xFF8d94ac)),
+                          Icon(Icons.star, size: 12, color: Color(0xFF8d94ac)),
+                          Icon(Icons.star, size: 12, color: Color(0xFF8d94ac)),
+                          Icon(Icons.star, size: 12, color: Color(0xFF8d94ac)),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            'đánh giá',
+                            style: TextStyle(
+                                color: Color(0xFF8d94ac), fontSize: 12),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text('19.990.000 đ'),
+                      Row(
+                        children: [
+                          Text('17.690.000 đ'),
+                          Container(
+                            child: Text('-12%'),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
               alignment: Alignment.topCenter,
               child: Container(
-                width: _isHovered ? double.infinity : 0.0,
-                height: _isHovered ? 3.0 : 0.0,
-                decoration: BoxDecoration(color: Color(0xFF3278f6)),
+                width: double.infinity,
+                height: _isHovered ? 3.0 : 1.0,
+                color: _isHovered ? Color(0xFF3278f6) : Color(0xFFededed),
               )),
           Align(
               alignment: Alignment.centerLeft,
               child: Container(
-                width: _isHovered ? 3.0 : 0.0,
-                height: _isHovered ? double.infinity : 0.0,
-                decoration: BoxDecoration(color: Color(0xFF3278f6)),
+                width: _isHovered ? 3.0 : 1.0,
+                height: double.infinity,
+                color: _isHovered ? Color(0xFF3278f6) : Color(0xFFededed),
               )),
           Align(
               alignment: Alignment.centerRight,
               child: Container(
-                width: _isHovered ? 3.0 : 0.0,
-                height: _isHovered ? double.infinity : 0.0,
-                decoration: BoxDecoration(color: Color(0xFF3278f6)),
+                width: _isHovered ? 3.0 : 1.0,
+                height: double.infinity,
+                color: _isHovered ? Color(0xFF3278f6) : Color(0xFFededed),
               )),
           Align(
               alignment: Alignment.bottomCenter,
               child: Container(
-                width: _isHovered ? double.infinity : 0.0,
-                height: _isHovered ? 3.0 : 0.0,
-                decoration: BoxDecoration(color: Color(0xFF3278f6)),
+                width: double.infinity,
+                height: _isHovered ? 3.0 : 1.0,
+                color: _isHovered ? Color(0xFF3278f6) : Color(0xFFededed),
               )),
-          Positioned.fill(
-            bottom: 0,
-            child: Padding(
-              padding: EdgeInsets.all(12),
-              child:
-                  Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-                Text("123"),
-              ]),
-            ),
-          )
         ],
       ),
     );
