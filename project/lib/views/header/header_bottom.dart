ddb9c837454aaf3e019784933f077df6a6907914
Ducanh2404 <1951060533@e.tlu.edu.vn>
2023-12-03 22:51:02 +0700

    update

--- project/lib/views/header/header_bottom.dart
+++ project/lib/views/header/header_bottom.dart
@@ -8,111 +8,6 @@ class HeaderBottom extends StatefulWidget {
 }
 
 class _HeaderBottomState extends State<HeaderBottom> {
-  OverlayEntry? overlayEntry;
-  int currentPageIndex = 0;
-  void createHighlightOverlay({
-    required AlignmentDirectional alignment,
-    required Color borderColor,
-  }) {
-    // Remove the existing OverlayEntry.
-    assert(overlayEntry == null);
-
-    overlayEntry = OverlayEntry(
-      // Create a new OverlayEntry.
-      builder: (BuildContext context) {
-        // Align is used to position the highlight overlay
-        // relative to the NavigationBar destination.
-        return SafeArea(
-          child: Align(
-            alignment: alignment,
-            heightFactor: 1.0,
-            child: DefaultTextStyle(
-              style: const TextStyle(
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-                fontSize: 14.0,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const Text('Tap here for'),
-                  Builder(builder: (BuildContext context) {
-                    switch (currentPageIndex) {
-                      case 0:
-                        return const Column(
-                          children: <Widget>[
-                            Text(
-                              'Explore page',
-                              style: TextStyle(
-                                color: Colors.red,
-                              ),
-                            ),
-                            Icon(
-                              Icons.arrow_downward,
-                              color: Colors.red,
-                            ),
-                          ],
-                        );
-                      case 1:
-                        return const Column(
-                          children: <Widget>[
-                            Text(
-                              'Commute page',
-                              style: TextStyle(
-                                color: Colors.green,
-                              ),
-                            ),
-                            Icon(
-                              Icons.arrow_downward,
-                              color: Colors.green,
-                            ),
-                          ],
-                        );
-                      case 2:
-                        return const Column(
-                          children: <Widget>[
-                            Text(
-                              'Saved page',
-                              style: TextStyle(
-                                color: Colors.orange,
-                              ),
-                            ),
-                            Icon(
-                              Icons.arrow_downward,
-                              color: Colors.orange,
-                            ),
-                          ],
-                        );
-                      default:
-                        return const Text('No page selected.');
-                    }
-                  }),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width / 3,
-                    height: 80.0,
-                    child: Center(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: borderColor,
-                            width: 4.0,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
-
-    // Add the OverlayEntry to the Overlay.
-    Overlay.of(context, debugRequiredFor: widget).insert(overlayEntry!);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -136,41 +31,31 @@ class _HeaderBottomState extends State<HeaderBottom> {
                 },
                 child:
                     const Image(image: AssetImage('img/logo.png'), width: 160)),
-            const SearchBarApp(),
+            Expanded(child: const SearchBarApp()),
             Row(
               children: [
-                Stack(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(right: 20.0),
-                      decoration: const BoxDecoration(
-                          border: Border(
-                              right: BorderSide(
+                Container(
+                  margin: const EdgeInsets.only(right: 20.0),
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          right: BorderSide(
+                    color: Colors.white,
+                    width: 1.0,
+                  ))),
+                  child: TextButton.icon(
+                      style: ButtonStyle(overlayColor: TransparentButton()),
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      icon: const Icon(
+                        Icons.person_outline,
                         color: Colors.white,
-                        width: 1.0,
-                      ))),
-                      child: TextButton.icon(
-                          style: ButtonStyle(overlayColor: TransparentButton()),
-                          onPressed: () {
-                            setState(() {
-                              currentPageIndex = 2;
-                            });
-                            createHighlightOverlay(
-                              alignment: AlignmentDirectional.bottomEnd,
-                              borderColor: Colors.orange,
-                            );
-                          },
-                          icon: const Icon(
-                            Icons.person_outline,
-                            color: Colors.white,
-                            size: 20.0,
-                          ),
-                          label: const Text('Tài khoản',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold))),
-                    ),
-                  ],
+                        size: 20.0,
+                      ),
+                      label: const Text('Tài khoản',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold))),
                 ),
                 TextButton.icon(
                     style: ButtonStyle(
