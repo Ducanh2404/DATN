08f8a5e548ca4889ae48d04bba90aeda67d60597
Ducanh <tranducanh2404@gmail.com>
2023-12-05 14:19:42 +0700

    update

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:project/all_imports.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
--- project/windows/flutter/generated_plugins.cmake
+++ project/windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
+  firebase_core
   gal
   pasteboard
   url_launcher_windows
