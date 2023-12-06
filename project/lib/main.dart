387d9fe851348cee64a99acde13ff3360cd4a106
Ducanh2404 <1951060533@e.tlu.edu.vn>
2023-12-06 20:39:06 +0700

    update

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:project/all_imports.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ApplicationState(),
+    builder: ((context, child) => const MyApp()),
+  ));
 }
 
 class MyApp extends StatelessWidget {
